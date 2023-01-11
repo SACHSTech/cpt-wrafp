42cf37a6bfc25c8e3c2ac5b57aad5786ee7ba19c
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2023-01-11 00:16:59 -0500

    Added filereader, only limited functionality

--- src/cpt/Datacollection.java
+++ src/cpt/Datacollection.java
@@ -2,21 +2,36 @@ package cpt;
 import java.io.*;
 
 public class Datacollection {
-    public static void main(String[] args) {
-        
-    }
+    
+    
 
-    private void ParseData(String fileName) {
-        BufferedReader thefile = new BufferedReader(new FileReader("human-development-index-escosura.csv"))
-        
-        String strStuff = "";
 
-        while (strStuff != null) {
-            strStuff = thefile.readLine();
-            if (strStuff != null) {
-                System.out.println("Output: " + strStuff);
+    public static void main(String[] args) throws IOException {
+        // Initialize
+        String filePath = "src\\cpt\\human-development-index-escosura.csv";
+
+        String currentLine = "";
+        
+        
+        try {
+            BufferedReader fileRead = new BufferedReader(new FileReader(filePath));
+            while (currentLine != null) {
+                currentLine = fileRead.readLine();
+                System.out.println(currentLine);
+                // if (currentLine != null) {
+                //     System.out.println("Output: " + currentLine);
+                // }
             }
+    
+    
+            fileRead.close();
         }
-        thefile.close();
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+   
+       
+        
+    
 }
