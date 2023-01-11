2cef93394d2f75678745ed98be40060da9db669d
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2023-01-11 10:58:44 -0500

    Added constructor to Datapoint.java

--- src/cpt/Datapoint.java
+++ src/cpt/Datapoint.java
@@ -1,6 +1,18 @@
 package cpt;
 
 public class Datapoint {
+
+    // Instance variables
+    private String countryName;
+    private String countryCode;
+    private int year;
+    private double HHDI;
+
+    public Datapoint(String theCountryName, String theCountryCode, int theYear, double theHHDI) {
+        this.countryName = theCountryName;
+        this.countryCode = theCountryCode;
+        this.year = theYear;
+        this.HHDI = theHHDI;
+    }
     
-   
 }
