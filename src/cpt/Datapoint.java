67a89544bdee7fa1cf80676ec6877111051c5b5f
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2023-01-24 01:47:48 -0500

    Added javadocs to Datapoint.java

--- src/cpt/Datapoint.java
+++ src/cpt/Datapoint.java
@@ -1,5 +1,9 @@
 package cpt;
 
+/**
+ * Datapoint class
+ * @author K.huang
+ */
 public class Datapoint {
 
     // Instance variables
@@ -8,6 +12,13 @@ public class Datapoint {
     private int year;
     private double HIHD;
 
+    /**
+     * Constructor - creates new instance of a datapoint
+     * @param theCountryName - the name of the country
+     * @param theCountryCode - the 3-letter country code (eg. USA)
+     * @param theYear - the year of the data
+     * @param theHIHD - the "quality of life" of that corresponding year
+     */
     public Datapoint(String theCountryName, String theCountryCode, int theYear, double theHIHD) {
         this.countryName = theCountryName;
         this.countryCode = theCountryCode;
@@ -15,22 +26,38 @@ public class Datapoint {
         this.HIHD = theHIHD;
     }
 
+    
+    /**
+     * @return country name
+     */
     public String getCountryName() {
         return countryName;
     }
 
+    /**
+     * @return country code
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * @return the year
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * @return the "quality of life" of that corresponding year
+     */
     public double getHIHD() {
         return HIHD;
     }
 
+    /**
+    * @return a formatted version of the Datapoint
+    */
     public String toString() {
         return getCountryName() + " (" + getCountryCode() + ") | Year: " + getYear() + " | HIHD: " + getHIHD() + "\n"; 
     }
